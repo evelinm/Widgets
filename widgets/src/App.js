8d7d0d34d46dcc676abd2e47fba58720ae176f06
evelinm <mandamurillo910@gmail.com>
2022-03-22 14:16:25 -0700

    initial setup

---
+++ widgets/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Accordion from './Components/Accordion';
+
+const  App = () => {
+    console.log("jola")
+    return <div>
+        <Accordion/>
+    </div>
+};
+export default App;
