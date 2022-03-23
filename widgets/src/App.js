5d435f447a7414d53a854d420b868818d6bf481a
evelinm <mandamurillo910@gmail.com>
2022-03-22 17:26:13 -0700

    accordion component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Accordion from './Components/Accordion';
 
+const items = [
+    {
+        title: "What is React?",
+        content: "React is a front end JavaScript FrameWork"
+    },
+    {
+        title: "Why use React?",
+        content: "React is a favorite JS library among engineers"
+    },
+    {
+        title: "How do you use React?",
+        content: "You use React by creating components"
+    }
+];
+
 const  App = () => {
-    console.log("jola")
+  
     return <div>
-        <Accordion/>
+        <Accordion items={items}/>
     </div>
 };
 export default App;
